502f11600334faf56856d0637b5cf8a1e7b6ac27
Muratcom <eksi038@gmail.com>
2018-02-25 14:46:33 +0300

    kendogrid ile satır seçimi yapması eklendi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,16 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonsModule } from '@progress/kendo-angular-buttons';
+import { GridModule } from '@progress/kendo-angular-grid';
+import { HttpClient, HttpClientModule, HttpClientJsonpModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
-
 
 @NgModule({
   declarations: [
@@ -12,9 +18,23 @@ import { HomeComponent } from './components/home/home.component';
     HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    BrowserAnimationsModule,
+    ButtonsModule,
+    GridModule,
+    HttpClientModule,
+    HttpClientJsonpModule,
+    ReactiveFormsModule
+
   ],
-  providers: [],
+  providers: [
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+
+
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { products } from './../../models/products';
+import { SelectableSettings } from '@progress/kendo-angular-grid';
 
+
+import { Component, OnInit, Inject } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { GridDataResult } from '@progress/kendo-angular-grid';
+import { State, process } from '@progress/kendo-data-query';
+import { map } from 'rxjs/operators/map';
+import { Observable } from 'rxjs/Observable';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  public gridData: any[] = products;
 
-  constructor() { }
-
-  ngOnInit() {
+  public onSelect(e) {
+console.log(e);
   }
+
+  constructor() {
+
+  }
+
+  public ngOnInit(): void {
+
+  }
+
 
 }
